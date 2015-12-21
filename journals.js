71e4b9979d99fbb7497a0ae703080f0fab2cdb07
jordandalt <jordan.a.dalton@gmail.com>
2015-12-21 14:21:37 -0500

    First commit for test application

---
+++ journals.js
@@ -0,0 +1,19 @@
+$.getJSON("journals.json", function(data) {
+        // since we are using jQuery, you don't need to parse response
+        drawTable(data);
+    }
+);
+
+function drawTable(data) {
+    for (var i = 0; i < data.length; i++) {
+        drawRow(data[i]);
+    }
+}
+
+function drawRow(rowData) {
+    var row = $("<tr />")
+    $("#journals").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.id + "</td>"));
+    row.append($("<td>" + rowData.firstName + "</td>"));
+    row.append($("<td>" + rowData.lastName + "</td>"));
+}
